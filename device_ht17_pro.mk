2ddd2cdad71170e1152d847d74c00620b88d9bad
vo-1 <vo-1@yandex.ru>
2017-01-17 11:12:53 +0300

    disable ninja

--- device_ht17_pro.mk
+++ device_ht17_pro.mk
@@ -4,7 +4,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 # Off ninja
-# USE_NINJA=false
+USE_NINJA=false
 
 $(call inherit-product-if-exists, vendor/homtom/ht17_pro/ht17_pro-vendor.mk)
 
