853f3127910920499453f2cbe3c50af25192d2aa
Vladimir Chernov <vlschernov@gmail.com>
2017-01-23 15:19:09 +0300

    fix build+patches

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -5,6 +5,8 @@ git apply -v ../../device/homtom/ht17_pro/patches/frameworks_av/0001-Disable-usa
 cd ../..
 cd system/core
 git apply -v ../../device/homtom/ht17_pro/patches/system_core/0001-remove-CAP_SYS_NICE-from-surfaceflinger-and-cln-logg.patch
+git apply -v ../../device/homtom/ht17_pro/patches/system_core/0003-Add-depreciated-symbols-needed-by-old-mali-blobs.patch
+git apply -v ../../device/homtom/ht17_pro/patches/system_core/0004-libnetutils-add-MTK-bits.patch
 #git apply -v ../../device/homtom/ht17_pro/patches/system_core/0002-Changes-for-more-level-log.patch
 cd ../..
 cd system/netd
